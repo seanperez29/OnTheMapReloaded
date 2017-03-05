7b406fe7d3df69498d004e2950ecb32cfa8aaa50
Sean Perez <sean.perez29@gmail.com>
2017-03-04 21:31:07 -0700

    Add functionality to forward geocode location and place pin on map with region

--- OnTheMapReloaded/MakePostViewController.swift
+++ OnTheMapReloaded/MakePostViewController.swift
@@ -7,25 +7,52 @@
 //
 
 import UIKit
+import MapKit
 
 class MakePostViewController: UIViewController {
 
     @IBOutlet weak var locationTextField: CustomTextField!
     @IBOutlet weak var mediaTextField: CustomTextField!
-    
-    
+    var placemark: CLPlacemark!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     @IBAction func findLocationPressed(_ sender: Any) {
+        if let location = locationTextField.text, location != "", let website = mediaTextField.text, website != "" {
+            forwardGeocoding(location)
+        } else {
+            UdacityClient.sharedInstance.displayErrorAlert(self, title: "Please enter a location and a valid website")
+        }
     }
-    
     
     @IBAction func cancelPressed(_ sender: Any) {
         let _ = navigationController?.popToRootViewController(animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showMap" {
+            let controller  = segue.destination as! UserPostMapViewController
+            controller.placemark = placemark
+            controller.mediaURL = mediaTextField.text
+        }
+    }
+    
+    func forwardGeocoding(_ address: String) {
+        CLGeocoder().geocodeAddressString(address) { (placemarks, error) in
+            if let placemark = placemarks?[0] {
+                self.placemark = placemark
+                performUIUpdatesOnMain {
+                    self.performSegue(withIdentifier: "showMap", sender: nil)
+                }
+            } else {
+                performUIUpdatesOnMain {
+                    UdacityClient.sharedInstance.displayErrorAlert(self, title: "Unable to process your location. Please try again!")
+                }
+            }
+        }
     }
 
 }
--- OnTheMapReloaded/UserPostMapViewController.swift
+++ OnTheMapReloaded/UserPostMapViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
 class UserPostMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var finishButton: UIButton!
+    var mediaURL: String!
+    var placemark: CLPlacemark!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        placePinOnMap(placemark)
     }
-
+    
+    @IBAction func finishButtonPressed(_ sender: Any) {
+        
+    }
+    
+    func placePinOnMap(_ location: CLPlacemark) {
+        mapView.addAnnotation(MKPlacemark(placemark: placemark))
+        let locationCoordinate = placemark.location!.coordinate as CLLocationCoordinate2D
+        setMapViewRegionAndScale(locationCoordinate)
+    }
+    
+    func setMapViewRegionAndScale(_ location: CLLocationCoordinate2D) {
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 
 }
